f63e2eb5360d875de0d6d85ce4dddbec9ba06d88
sai-jagadeesh-2002 <160568636+sai-jagadeesh-2002@users.noreply.github.com>
2024-03-12 15:02:35 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
+import RegisterPage from './pages/RegisterPage'
+import LoginPage from './pages/LoginPage'
+import DashboardPage from './pages/DashboardPages'
+
+function App() {
+  return (
+    <Routes>    
+      <Route exact path='/' element={<RegisterPage />} />
+      <Route exact path='/login' element={<LoginPage />} />
+      <Route exact path='/dashboard' element={<DashboardPage />} />
+    </Routes>
+  )
+}
+
+export default App
---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import RegisterForm from '../components/RegisterForm'
+import Navbar from '../components/Navbar'
+
+export default function RegisterPage() {
+  return (
+    <div>
+      <>
+      <Navbar/>
+      <RegisterForm/>
+      </>
+    </div>
+  )
+}
